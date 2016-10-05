252137e525074d86bfdefe980adb7a9f71c6bba0
Aakaash Jois <aakaashjois@gmail.com>
2016-10-05 23:32:36 +0530

    - Refactored code.

--- app/src/main/java/com/example/admin/skyway/NewCustomer.java
+++ app/src/main/java/com/example/admin/skyway/NewCustomer.java
@@ -1,79 +1,71 @@
 package com.example.admin.skyway;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 public class NewCustomer extends AppCompatActivity {
 
-    String payment="";
-    String details="";
+    private EditText nm1;
+    private EditText ad1;
+    private EditText ph1;
+    private EditText alt1;
+    private EditText tent1;
+    private CheckBox m;
+    private CheckBox y;
+    private CheckBox h;
+    private Spinner spinner;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_customer);
-
-        //////////////////////////////////////////////////////////////////////////////
-        Spinner spinner = (Spinner) findViewById(R.id.documents_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        initUi();
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.documents_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-
-    }
-public void getdata(View v)
-{
-    EditText nm1 = (EditText)findViewById(R.id.nm);
-    EditText ad1 = (EditText)findViewById(R.id.address);
-    EditText ph1 = (EditText)findViewById(R.id.phone_no);
-    EditText alt1 = (EditText)findViewById(R.id.altphone_no);
-    EditText tent1 = (EditText)findViewById(R.id.tent);
-    CheckBox m=(CheckBox)findViewById(R.id.monthly) ;
-    CheckBox y=(CheckBox)findViewById(R.id.full) ;
-    CheckBox h=(CheckBox)findViewById(R.id.half) ;
-    Spinner mySpinner = (Spinner)findViewById(R.id.documents_spinner);
-
-    String doc = mySpinner.getSelectedItem().toString();
-    String name = nm1.getText().toString();
-    String addr = ad1.getText().toString();
-    String phno = ph1.getText().toString();
-    String altno = alt1.getText().toString();
-    String tentof = tent1.getText().toString();
-    if(m.isChecked())
-    {
-        payment+=m.getText().toString();
-    }
-    if(h.isChecked())
-    {
-        payment+=h.getText().toString();
-    }
-    if(y.isChecked())
-    {
-        payment+=y.getText().toString();
     }
 
-    details+="\nName:"+name+"\nAddress"+addr+"\nPhone no:"+phno+"\nAlternate no:"+altno+"\nTenent of:"+tentof+"\ndocuments:"+doc+"\nPayment Mode:"+payment;
-}
-    public void submit(View v)
-    {
-        getdata(v);
-            Intent email = new Intent(Intent.ACTION_SEND);
-            email.putExtra(Intent.EXTRA_EMAIL, new String[]{"amarn19@gmail.com"});
-            email.putExtra(Intent.EXTRA_SUBJECT, "NewCustomerDetails");
-            email.putExtra(Intent.EXTRA_TEXT,details);
-            email.setType("message/rfc822");
-            startActivity(Intent.createChooser(email, "Choose an Email client :"));
+    private void initUi() {
 
+        nm1 = (EditText) findViewById(R.id.nm);
+        ad1 = (EditText) findViewById(R.id.address);
+        ph1 = (EditText) findViewById(R.id.phone_no);
+        alt1 = (EditText) findViewById(R.id.altphone_no);
+        tent1 = (EditText) findViewById(R.id.tent);
+        m = (CheckBox) findViewById(R.id.monthly);
+        y = (CheckBox) findViewById(R.id.full);
+        h = (CheckBox) findViewById(R.id.half);
+        spinner = (Spinner) findViewById(R.id.documents_spinner);
     }
 
+    public String getdata() {
+
+        final StringBuilder paymentStringBuilder = new StringBuilder();
+        if (m.isChecked()) paymentStringBuilder.append(m.getText().toString());
+        if (h.isChecked()) paymentStringBuilder.append(h.getText().toString());
+        if (y.isChecked()) paymentStringBuilder.append(y.getText().toString());
+        return "\nName: " + nm1.getText().toString() +
+                "\nAddress: " + ad1.getText().toString() +
+                "\nPhone No.: " + ph1.getText().toString() +
+                "\nAlternate No.: " + alt1.getText().toString() +
+                "\nTenant of: " + tent1.getText().toString() +
+                "\nDocuments: " + spinner.getSelectedItem().toString() +
+                "\nPayment Mode: " + paymentStringBuilder.toString();
+    }
+
+    public void submit(final View v) {
+        final Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{"amarn19@gmail.com"});
+        email.putExtra(Intent.EXTRA_SUBJECT, "NewCustomerDetails");
+        email.putExtra(Intent.EXTRA_TEXT, getdata());
+        email.setType("message/rfc822");
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
+    }
 }
--- app/src/main/java/com/example/admin/skyway/SplashActivity.java
+++ app/src/main/java/com/example/admin/skyway/SplashActivity.java
@@ -2,21 +2,20 @@ package com.example.admin.skyway;
 
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         new Handler().postDelayed(new Runnable() {
             public void run() {
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 finish();
             }
-        },1000);//1000 seconds
+        }, 1000);
     }
 }
